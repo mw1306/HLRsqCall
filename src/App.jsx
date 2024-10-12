198682a586f83e9a2030e26dbc293de6e3709fe2
mw1306 <mal.waite23@gmail.com>
2024-10-12 13:07:24 +1100

    changes for logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import hlolgo from "./assets/hllogo.jpeg":
+import hllogo from "./assets/hllogo.jpeg":
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={hllogo} className="logo" alt="React hl logologo" />
+        <img src={hllogo} className="logo" alt="R hl logo" />
 
         <h1>Hello from Amplify</h1>
       </header>
